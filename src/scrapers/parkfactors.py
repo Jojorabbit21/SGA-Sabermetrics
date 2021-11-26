b7dfe523fbff99573c4324f087772bc1ac6114f5
Gwanghee Jo <92922343+Jojorabbit21@users.noreply.github.com>
2021-11-26 09:30:14 +0900

    Update parkfactors.py

--- src/scrapers/parkfactors.py
+++ src/scrapers/parkfactors.py
@@ -8,4 +8,4 @@ from src.utils.dictionaries import *
 def get_park_factors():
   park_factors = pd.read_html(URL_LINEUPS['PARK_FACTORS'],encoding='utf-8')
   park_factors = park_factors[0].drop(['Game', 'Time (GMT)'], axis=1)
-  park_factors.to_csv('./rawfish/park_factors.csv',',',encoding='utf-8')
+  return park_factors
