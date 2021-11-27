84f3825b2797aa42a6209ffecb6b7e9b1a5d6a36
Gwanghee Jo <92922343+Jojorabbit21@users.noreply.github.com>
2021-11-27 20:15:28 +0900

    Minor #1127

--- src/scrapers/depthchart.py
+++ src/scrapers/depthchart.py
@@ -43,5 +43,29 @@ def get_team_depthcharts():
       t = i.text.replace(' Depth Chart','')
       team_name.append(t)
     
-    # body > app-root > div.outlet-container > app-depth-charts > div > div > div.col-12.before-text-margin > div > div:nth-child(1) > app-single-dp-item > div > table > tbody > tr:nth-child(3) > td:nth-child(2) > app-player-link > a > span > span.long-player-name
-                                                                                                                  # nth-child 팀 선택 1~30                                      tr:nth-child(3-10) #DH는 11
+    # for i in range(1,31):
+    for i in range(1,2):
+      selector = 'div.col-12.before-text-margin > div > div:nth-child(' + str(i) + ') > app-single-dp-item > div > table.multi-row-data-table.t-stripped'
+      table = soup.select_one(selector=selector)
+      dataframe_row = []
+      for a in range(3,11):
+        for b in range(2,5):
+          # tbody > tr:nth-child(3) > td:nth-child(2)
+          selector = "tbody > tr:nth-child(" + str(a) + ") > td:nth-child(" +str(b) + ")"
+          table_data = table.select_one(selector)
+          print(table_data.text)
+      #     if table_data:
+      #       long_name = table_data.select_one('span.long-player-name')
+      #       if long_name:
+      #         dataframe_row.append(long_name.text)
+      #     else:
+      #       dataframe_row.append('None')
+      # print(dataframe_row)
+          
+          
+    # div.col-12.before-text-margin > div > div:nth-child(1) > app-single-dp-item > div > table > tbody > tr:nth-child(3) > td:nth-child(2) > app-player-link > a > span > span.long-player-name
+                                                                                                                    # nth-child 팀 선택 1~30                                      tr:nth-child(3-10) #DH는 1                                                                                                  
+    # DataFrame Structure
+    #           C-1             C-2               C-3     ...
+    #   ARI     Carson Kelly    Daulton Varsho    None
+    #   ATL     Pedro Severino  Austin Wynns      Orlando Arcia ...
